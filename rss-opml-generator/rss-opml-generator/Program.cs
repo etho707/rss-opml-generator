949348c651e5677c6f4f1271316d46da6f14d6cb
Etho <drxkane@gmail.com>
2021-09-02 13:41:11 +0300

    1

---
+++ rss-opml-generator/rss-opml-generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rss_opml_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
